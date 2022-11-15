56e9c71cc2a6d06cf24ab2f4ec7d610e52440140
Kaique Santos <kaique.santos20042018@gmail.com>
2022-11-15 14:19:58 -0300

    validacoes

--- Models/Computer.cs
+++ Models/Computer.cs
@@ -1,9 +1,15 @@
 namespace MvcLabManager.Models;
+using System.ComponentModel.DataAnnotations;
 
 public class Computer
 {
     public int? Id { get; set; }
+
+    [Required(ErrorMessage = "Preencha o campo Ram")]
+    [StringLength(5, ErrorMessage = "O campo Ram pode ter no máximo 5 caracteres")]
     public string? Ram { get; set; }
+    
+    [Required(ErrorMessage = "Preencha o campo Processor")]
     public string? Processor { get; set; }
 
     public Computer() { }
--- Models/Lab.cs
+++ Models/Lab.cs
@@ -1,10 +1,19 @@
 namespace MvcLabManager.Models;
+using System.ComponentModel.DataAnnotations;
 
 public class Lab
 {
     public int? Id { get; set; }
+
+    [Required(ErrorMessage = "Preencha o campo Number")]
+    [Range(1, 100, ErrorMessage = "O campo Number necessita estar no intervalo de 1 a 100")]
     public int? Number { get; set; }
+
+    [Required(ErrorMessage = "Preencha o campo Name")]
     public string? Name { get; set; }
+
+    [Required(ErrorMessage = "Preencha o campo Block")]
+    [StringLength(1, ErrorMessage = "O campo Block pode ter no máximo 1 caracter")]
     public string? Block { get; set; }
 
     public Lab() { }
